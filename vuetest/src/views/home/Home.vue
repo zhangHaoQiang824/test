30de706e426f10aa94199bdb2c87fe9aaddd2dc8
zhangHaoQiang824 <1923929126@qq.com>
2024-02-24 19:24:38 +0800

    main

--- vuetest/src/views/home/Home.vue
+++ vuetest/src/views/home/Home.vue
@@ -35,7 +35,10 @@
   <el-button type="warning" icon="el-icon-star-off" circle></el-button>
   <el-button type="danger" icon="el-icon-delete" circle></el-button>
 </el-row>
-
+<i class="el-icon-edit"></i>
+<i class="el-icon-share"></i>
+<i class="el-icon-delete"></i>
+<el-button type="primary" icon="el-icon-search">搜索</el-button>
 
   
   </div>
